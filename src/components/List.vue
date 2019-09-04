c1057b8b1320d1487084dfd3066d58829c12d8e1
wanghc00 <306093226@qq.com>
2019-09-04 22:34:22 +0800

    拆分出input

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="list">
+    <label>
+      <input name="done-todo" type="checkbox" class="done-todo" />
+    </label>
+    <span>{{$store.getters.getValueByIndex(transIndex)}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    transIndex: Number
+  }
+};
+</script>
+
+<style>
+input[type="checkbox"].done-todo {
+  margin: 5px 5px 2px 0;
+}
+</style>
